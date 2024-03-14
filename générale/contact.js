003802d095a1e576910e4f4c6145f9743951cb15
NathanNOVARESE <nathan.novarese@gmail.com>
2024-03-14 12:20:49 +0100

    ajout page profile

--- générale/contact.js
+++ générale/contact.js
@@ -25,3 +25,16 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+let map;
+
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+
+  map = new Map(document.getElementById("map"), {
+    center: { lat: 43.615171970344804, lng: 7.05530506998268},
+    zoom: 18,
+  });
+}
+
+initMap();
