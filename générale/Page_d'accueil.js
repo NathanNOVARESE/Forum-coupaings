a63ecaed9d2871898aeb7caf9b93078d58d084ff
TomBallauri <144777722+TomBallauri@users.noreply.github.com>
2024-03-14 12:26:17 +0100

    4

--- générale/Page_d'accueil.js
+++ générale/Page_d'accueil.js
@@ -1,28 +1,57 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var slider = document.querySelector('.slider');
-    var sliderWrapper = document.querySelector('.slider-wrapper');
+    var menuButtonJeux = document.getElementById('menuButtonJeux');
+    var menuJeux = document.getElementById('menuJeux');
 
-    // Tableau de textes correspondant aux diapositives
-    var slideTexts = [
-        "Cloud                                                                                         Final Fantasy 7",
-        "Link                                                                                         Zelda Breath of The Wild",
-        "Geralt                                                                                         The Witcher 3",
-        "Ichiban                                                                                         Yakuza Infinite Wealth",
-    ];
-
-    // Mettre à jour le texte initial
-    updateSliderText();
-
-    slider.addEventListener('scroll', function () {
-        // Calculez l'index de la diapositive actuellement visible
-        var index = Math.round(slider.scrollLeft / slider.clientWidth);
-
-        // Mettre à jour le texte du slider avec le texte correspondant à la diapositive actuelle
-        updateSliderText(index);
+    menuButtonJeux.addEventListener('click', function () {
+        toggleMenu(menuJeux);
     });
 
-    function updateSliderText(index = 0) {
-        // Mettre à jour le contenu du pseudo-élément ::before avec le texte correspondant à la diapositive actuelle
-        sliderWrapper.style.setProperty('--slide-text', '"' + slideTexts[index] + '"');
+    var menuButtonTopics = document.getElementById('menuButtonTopics');
+    var menuTopics = document.getElementById('menuTopics');
+
+    menuButtonTopics.addEventListener('click', function () {
+        toggleMenu(menuTopics);
+    });
+
+    function toggleMenu(menu) {
+        menu.classList.toggle('show'); // Ajoute ou supprime la classe 'show'
     }
+
+
+    document.addEventListener('DOMContentLoaded', function () {
+        var slider = document.querySelector('.slider');
+        var sliderWrapper = document.querySelector('.slider-wrapper');
+    
+        // Tableau de textes correspondant aux diapositives
+        var slideTexts = [
+            "Cloud                                                                                         Final Fantasy 7",
+            "Link                                                                                         Zelda Breath of The Wild",
+            "Geralt                                                                                         The Witcher 3",
+            "Ichiban                                                                                         Yakuza Infinite Wealth",
+        ];
+    
+        // Mettre à jour le texte initial
+        updateSliderText();
+    
+        slider.addEventListener('scroll', function () {
+            // Calculez l'index de la diapositive actuellement visible
+            var index = Math.round(slider.scrollLeft / slider.clientWidth);
+    
+            // Mettre à jour le texte du slider avec le texte correspondant à la diapositive actuelle
+            updateSliderText(index);
+        });
+    
+        function updateSliderText(index = 0) {
+            // Mettre à jour le contenu du pseudo-élément ::before avec le texte correspondant à la diapositive actuelle
+            sliderWrapper.style.setProperty('--slide-text', '"' + slideTexts[index] + '"');
+        }
+    });
+    
+    // Fermer le menu si l'utilisateur clique en dehors du menu
+    document.addEventListener('click', function (event) {
+        if (!menuJeux.contains(event.target) && event.target !== menuButtonJeux && !menuTopics.contains(event.target) && event.target !== menuButtonTopics) {
+            menuJeux.classList.remove('show'); // Assurez-vous que la classe est supprimée si on clique en dehors du menu Jeux
+            menuTopics.classList.remove('show'); // Assurez-vous que la classe est supprimée si on clique en dehors du menu Topics
+        }
+    });
 });
